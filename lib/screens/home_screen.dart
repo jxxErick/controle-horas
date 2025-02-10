1370acdda19116157993c4b86a57697c16d9eaa4
ericksdantas144 <ericksdantas144@gmail.com>
2025-02-10 20:16:09 -0300

    Implements: init construction of home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../models/hour.dart';
+
 class HomeScreen extends StatefulWidget {
   final User user;
 
