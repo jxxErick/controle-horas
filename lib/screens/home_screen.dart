ab7f1654500153380283cc4957864f52ddbb13d1
ericksdantas144 <ericksdantas144@gmail.com>
2025-02-23 21:26:33 -0300

    Implements: hours helper in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -72,7 +72,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               size: 56,
                             ),
                             title: Text(
-                                "Data: ${model.data}   Horas: ${HoursHelper.minutesToHours(model.minutos)}"),
+                                "Data: ${model.data} - Horas: ${HoursHelper.minutesToHours(model.minutos)}"),
                             subtitle: Text(model.descricao!),
                           )
                         ],
